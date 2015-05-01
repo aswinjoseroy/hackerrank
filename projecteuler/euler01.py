83c1227fd4498d1763c153ace0070604707a1885
Igor Barinov <bis@hintsolutions.ru>
2015-04-30 20:59:12 -0700

    arithmetic function for euler01. test passed 100 points

--- projecteuler/euler01.py
+++ projecteuler/euler01.py
@@ -5,6 +5,8 @@
 #number of testcases
 t = int(input())
 
+#bruteforce
+#thanks to http://www.mathblog.dk/project-euler-problem-1/
 def calculate(n):
 	i = 0
 	result = 0
@@ -13,8 +15,20 @@ def calculate(n):
 			result += i
 		i += 1
 	return result
+
+
+# arithmetic
+
+def calculate2(n):
+	result = sumDivisible(3,n - 1)+sumDivisible(5,n - 1)-sumDivisible(15,n - 1)
+	return result
+def sumDivisible(n, p):
+	return n*(p/n)*((p/n)+1)/2
 		
 for i in range(t):
 	n = int(input())
-	print calculate(n)
+	# brute force O(n)
+	#print calculate(n)
+	# arithmetic O(1)
+	print calculate2(n)
 	
