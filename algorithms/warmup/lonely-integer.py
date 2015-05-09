da2e5c7dfa524206391b3b66dbd36f010436b3c0
Igor Barinov <bis@hintsolutions.ru>
2015-05-08 23:47:41 -0700

    .

---
+++ algorithms/warmup/lonely-integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/py
+def lonelyinteger(a):
+	answer = 0
+	return answer
+	
+if __name__ == '__main__':
+	a = input()
+	b = map(int, raw_input().strip().split(" "))
+	print lonelyinteger(b)
